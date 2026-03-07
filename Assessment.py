d2bfe584626dd25a285cf63a903bab59898f543a
Uttam Anand <https://github.com/young-sasuke>
2026-03-07 17:42:34 +0530

    Refactor

--- Assessment.py
+++ Assessment.py
@@ -1,6 +1,5 @@
 # import time
 # from selenium import webdriver
-# from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.action_chains import ActionChains
 # from selenium.webdriver.support.ui import Select
 
